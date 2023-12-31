d7da2735d05834dd50d2531321f2f82d3add9675
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-31 16:29:12 +0100

    fetching

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,5 +1,23 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const backendUrl = "http://localhost:4887";
+
 export const PageWelcome = () => {
+	const [backendAppName, setBackendAppName] = useState("");
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(`${backendUrl}`);
+			const _backendAppName = response.data;
+			setBackendAppName(_backendAppName);
+		})();
+	}, []);
+
 	return (
-		<p>This is the welcome page.</p>
-	)
-}
+		<>
+			<p>This is the welcome page.</p>
+			<p>Backend app name is: {backendAppName}</p>
+		</>
+	);
+};
